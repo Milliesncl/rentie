243d50cc0ff8118dcb584dc2be5aecd9d6b60a7c
Meagan Butters <meagan.butters@hotmail.com>
2020-08-26 18:06:57 -0300

    Basic method actions set up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,37 @@
 class TasksController < ApplicationController
-  def show
-    @task = Task.find(params[:id]
-  end
-
   def index
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(params_task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
 
-  def task_params
-
+  def params_task
+    params.require(:task).permit(:title, :description, :expense, :start_date, :end_date)
   end
 end
