32e017e7445c8c0d2c897e3c727a94352ce8d85f
Meagan Butters <meagan.butters@hotmail.com>
2020-08-26 18:36:58 -0300

    Default value of task is now nil/false

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ user1 = User.create!(email: "francesca@email.com", password: "123456", first_nam
 user2 = User.create!(email: "kyle@email.com", password: "123456", first_name: "Kyle", last_name: "Masterson")
 user3 = User.create!(email: "millie@email.com", password: "123456", first_name: "Millie", last_name: "Senecal")
 user4 = User.create!(email: "meagan@email.com", password: "123456", first_name: "Meagan", last_name: "Butters")
+
+task1 = Task.create!(title: "Plumber needed", description: "My toilet exploded",urgency: "High")
+task2 = Task.create!(title: "Electrician needed", description: "Light switch doesn't work",urgency: "Medium")
