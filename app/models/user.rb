1f62adee1955182ea606a674ec1710443bdc7934
Millie Senecal <millie.senecal@gmail.com>
2020-08-26 17:07:23 -0400

    add first_name last_name fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   has_many :buildings
   has_many :contractors
   has_many :tasks, through: :units
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
 end
