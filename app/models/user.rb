055701fa890ef3536972b9f1028f2562169b795d
Meagan Butters <meagan.butters@hotmail.com>
2020-08-26 17:20:40 -0300

    Fixed bug inside user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :buildings, :contractors, :units, :tasks
+  has_many :buildings
+  has_many :contractors
+  has_many :tasks, through: :units
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
