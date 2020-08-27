333f051fdedc21d9d5ddd463da713ab138440656
Meagan Butters <meagan.butters@hotmail.com>
2020-08-27 12:47:51 -0300

    Removed name column from contractor, added first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_151609) do
+ActiveRecord::Schema.define(version: 2020_08_27_154523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,8 @@ ActiveRecord::Schema.define(version: 2020_08_27_151609) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["user_id"], name: "index_contractors_on_user_id"
   end
 
