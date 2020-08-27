11fbef80b7d7be7a0057fd291b05d67bc44d1a71
Millie Senecal <millie.senecal@gmail.com>
2020-08-27 15:22:58 -0400

    edit views and fix edit for units

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -22,13 +22,17 @@ class UnitsController < ApplicationController
   end
 
   def edit
+    @building = Building.find(params[:building_id])
     @unit = Unit.find(params[:id])
   end
 
   def update
     @unit = Unit.find(params[:id])
-    if Unit.update(params_unit)
-      redirect_to building_unit_path([@building, @unit])
+    @building = Building.find(params[:building_id])
+    @unit.building = @building
+
+    if @unit.update(params_unit)
+      redirect_to building_unit_path(@building, @unit)
     else
       render :edit
     end
@@ -42,6 +46,6 @@ class UnitsController < ApplicationController
 
   private
   def params_unit
-    params.require(:unit).permit(:unit_number, :purchase_price, :payment_method, :renewal_date, :rent_amount, :payment_date)
+    params.require(:unit).permit(:unit_number, :purchase_price, :payment_method, :renewal_date, :rent_amount, :payment_date, photos: [])
   end
 end
