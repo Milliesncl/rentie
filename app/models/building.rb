e7382bd0bc0f515f8954b1edfcb97df147b623c9
Millie Senecal <millie.senecal@gmail.com>
2020-08-27 13:55:39 -0400

    edit views and seeds for buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,7 @@
 class Building < ApplicationRecord
   belongs_to :user
   has_many :units, dependent: :destroy
+  has_one_attached :photo
 
-  validates :address, :mortgage, :purchase_price, :purchase_date , presence: true
+  validates :address, :mortgage, :purchase_price, :purchase_date, presence: true
 end
