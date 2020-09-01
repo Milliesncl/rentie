035674b441844670f5614ce2c22c53759fc6b541
Millie Senecal <millie.senecal@gmail.com>
2020-09-01 19:56:00 -0400

    fix the bug for the js dounhut

--- app/javascript/plugins/unit_expenses.js
+++ app/javascript/plugins/unit_expenses.js
@@ -1,41 +1,45 @@
 import Chart from 'chart.js';
 
 const createUnitExpensesChart = () => {
-  const unitExpenseChartElement = document.getElementById("unit-expense-chart");
-  if (unitExpenseChartElement === null)
+  const unitExpenseChartElements = document.querySelectorAll("#unit-expense-chart");
+  if (unitExpenseChartElements === null)
     return;
-  const unitExpenseData = JSON.parse(unitExpenseChartElement.dataset.unitExpenseData);
-  const data = {
-    datasets: [{
-        data: unitExpenseData[1],
-        backgroundColor: ["#F4F4F4", "#13547A", "#80D0C7", "#07A3B2", "#D9ECC7", "#009BA5"]
-    }],
+  unitExpenseChartElements.forEach(
+    function(element) {
+      const unitExpenseData = JSON.parse(element.dataset.unitExpenseData);
+      const data = {
+        datasets: [{
+            data: unitExpenseData[1],
+            backgroundColor: ["#F4F4F4", "#13547A", "#80D0C7", "#07A3B2", "#D9ECC7", "#009BA5"]
+        }],
 
-  // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: unitExpenseData[0]
-  };
-  const unitExpensesChart = new Chart(unitExpenseChartElement, {
-    type: 'doughnut',
-    data: data,
-    options: {
-      legend: {
-        boxWidth: 20,
-      },
-      tooltips: {
-        mode: 'index',
-        intersect: false,
-        callbacks: {
-          label: function(tooltipItems, data) {
-            return ` ${data.labels[tooltipItems.index]}: $${data.datasets[0].data[tooltipItems.index]}`;
-          }
-        },
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true
-      },
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: unitExpenseData[0]
+      };
+      const unitExpensesChart = new Chart(element, {
+        type: 'doughnut',
+        data: data,
+        options: {
+          legend: {
+            boxWidth: 20,
+          },
+          tooltips: {
+            mode: 'index',
+            intersect: false,
+            callbacks: {
+              label: function(tooltipItems, data) {
+                return ` ${data.labels[tooltipItems.index]}: $${data.datasets[0].data[tooltipItems.index]}`;
+              }
+            },
+          },
+          hover: {
+            mode: 'nearest',
+            intersect: true
+          },
+        }
+      });
     }
-  });
+  );
 };
 
 export { createUnitExpensesChart };
